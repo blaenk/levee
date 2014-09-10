f91d3614e190a524a13ccc97d9e146458f3d6cee
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-09-10 16:37:58 -0700

    already using authorize macro

--- src/clj/levee/models/trackers.clj
+++ src/clj/levee/models/trackers.clj
@@ -6,30 +6,19 @@
     [levee.db :as db]))
 
 (defn edit-tracker [id req]
-  (if (users/admin? req)
-    (do
-      (db/update-tracker id
-        (clojure.walk/keywordize-keys (:json-params req)))
-      (response/response (db/get-trackers)))
-    (response/response {:error "lacks authorization"})))
+  (db/update-tracker id
+                     (clojure.walk/keywordize-keys (:json-params req)))
+  (response/response (db/get-trackers)))
 
 (defn edit-tracker-page [id req]
-  (if (users/admin? req)
-    (handle-resource req (db/get-tracker-by-id id))
-    (response/redirect "/trackers")))
+  (handle-resource req (db/get-tracker-by-id id)))
 
 (defn remove-tracker [id req]
-  (if (users/admin? req)
-    (do
-      (db/remove-tracker id)
-      (response/response (db/get-trackers)))
-    (response/response {:error "lacks authorization"})))
+  (db/remove-tracker id)
+  (response/response (db/get-trackers)))
 
 (defn create-tracker [req]
-  (if (users/admin? req)
-    (do
-      (db/insert-tracker
-        (clojure.walk/keywordize-keys (:json-params req)))
-      (response/response (db/get-trackers)))
-    (response/response {:error "lacks authorization"})))
+  (db/insert-tracker
+    (clojure.walk/keywordize-keys (:json-params req)))
+  (response/response (db/get-trackers)))
 
