159b1043497262c43dff73881c03ecb1d829cfc3
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-12-22 16:19:07 -0800

    don't even bother parsing the uri

--- src/clj/levee/models/downloads.clj
+++ src/clj/levee/models/downloads.clj
@@ -67,7 +67,7 @@
 
 (defn get-trackers [hash]
   (let [trackers (rtorrent/trackers hash :get_url)]
-    (map #(.getHost (java.net.URI. (:url %))) trackers)))
+    (map :url trackers)))
 
 (defn get-downloads []
   (let [downloads (rtorrent/torrents "main"
